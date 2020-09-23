e18e0dea234c92636ed95b0bc0a40a990a65b4a2
ABerrorch <784031469@qq.com>
2020-09-23 22:07:02 +0800

    对Server进行修改，主要修改了清除流记录的方式。设置在timeOut时间后删除（目前设置的timeOut时间是5s）。
    同时顺便修改了应答机制，在timeOut结束之前每接受到一个UDP包发一个ACK。

--- src/Server.java
+++ src/Server.java
@@ -3,6 +3,7 @@ import model.TransmissionData;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,9 +18,13 @@ public class Server {
             DatagramSocket socket;
             Map<Integer, TransmissionData> tDataMap = new HashMap<>();
             Map<Integer, Integer> receiveDataMap = new HashMap<>();
+            Map<Integer, Long> timeOutMap = new HashMap<>(); // 在这个Map中的数据将在10秒后删除
+            Map<Integer, DatagramSocket> socketMap = new HashMap<>(); // 在这个Map中的数据将在10秒后删除
 
             System.out.println("Server is running and listen to " + Constant.SERVER_PORT);
             socket = new DatagramSocket(Constant.SERVER_PORT);
+
+            long lastCleanTime = System.currentTimeMillis();
 
             // noinspection InfiniteLoopStatement 服务器本就是一直持续的,
             while(true){
@@ -29,9 +34,9 @@ public class Server {
                     socket.receive(packet);
                     byte[] data = packet.getData();
 
-                    // 这里应该没有问题了
+                    // 解析收到的数据
                     String[] splits = new String(data).split(TransmissionData.SF);
-                    if(splits.length < 8){
+                    if(splits.length < Constant.NUMBER_OF_DATA_PREFIX){ // 不合法数据直接抛弃
                         System.out.println("The UDP format is illegal.");
                         continue;
                     }
@@ -39,8 +44,16 @@ public class Server {
                             Arrays.copyOfRange(splits,0, Constant.NUMBER_OF_DATA_PREFIX)
                     );
 
+
                     int newSum = 0,key = 65535 - tData.hashCode();
-                    // 假如不包含这个键，则记录新的tData
+                    // 1. 首先判断是否在在TimeOutMap中，假如在TimeOutMap则立即应答ACK,并且更新TimeOutMap时间
+                    if(timeOutMap.containsKey(key)){
+                        ACK(tData,socketMap.get(key));
+                        timeOutMap.put(key,System.currentTimeMillis());
+                        continue; // 不执行后面的部分
+                    }
+
+                    // 2. 然后判断现在的tDataMap是否包含这个键。假如不包含这个键，则添加新的记录到tDataMap和和receiveDataMap中
                     if(!tDataMap.containsKey(key)){
                         System.out.printf(
                                 "Flow(%d,%d) from %s:%d is established\n",
@@ -54,15 +67,21 @@ public class Server {
                     }
                     receiveDataMap.put(key,newSum);
 
-                    // 假如已经接收到的数据量超过的data_size,这发送应答信号
+                    // 3. 假如在receive记录中记录的已经接收到的数据量超过了data_size,则发送应答信号。
+                    // 并且在timeOutMap和socketMap中添加记录
                     if(newSum > tData.data_size){
                         System.out.printf(
                                 "Flow(%d,%d) from %s:%d is completed\n",
                                 tData.coflow_id,tData.flow_id,tData.src_ip,tData.src_port
                         );
-                        ACK(tData);
-                        tDataMap.remove(key);
-                        receiveDataMap.remove(key);
+                        socketMap.put(key, ACK(tData,null)); // 备份socket
+                        timeOutMap.put(key,System.currentTimeMillis());
+                    }
+
+                    // 周期性删除过时记录
+                    if(System.currentTimeMillis() - lastCleanTime > Constant.TIME_OUT_LIMIT){
+                        cleanMap(tDataMap,receiveDataMap,timeOutMap,socketMap);
+                        lastCleanTime = System.currentTimeMillis();
                     }
 
                 } catch (IOException e) {
@@ -86,26 +105,58 @@ public class Server {
      * 发送应答信号
      * @param tData TransmissionData
      */
-    private static void ACK(TransmissionData tData){
-        try {
-            // 传输到Client的65535 - hash处
-            DatagramSocket socket = new DatagramSocket(65535 - tData.hashCode());
-            for(int i = 0; i < Constant.ACK_NUMBER; i++){
-                try {
-                    byte[] buff = Arrays.copyOf(tData.toString().getBytes(), Constant.BUFF_SIZE);
-                    // Client的接受端口是10000 + hash
-                    DatagramPacket packet = new DatagramPacket(
-                            buff, 0, buff.length,
-                            InetAddress.getByName(tData.src_ip),
-                            10000 + tData.hashCode()
-                    );
-                    socket.send(packet);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+    private static DatagramSocket ACK(TransmissionData tData,DatagramSocket socket) throws IOException{
+        if (socket == null) {
+            try {
+                // 传输到Client的65535 - hash处
+                socket = new DatagramSocket(65535 - tData.hashCode());
+            } catch (SocketException e) {
+                e.printStackTrace();
             }
-        } catch (SocketException e) {
-            e.printStackTrace();
+        }
+        byte[] buff = Arrays.copyOf(tData.toString().getBytes(), Constant.BUFF_SIZE);
+        // Client的接受端口是10000 + hash
+        DatagramPacket packet = new DatagramPacket(
+            buff, 0, buff.length,
+            InetAddress.getByName(tData.src_ip),
+            10000 + tData.hashCode()
+        );
+        assert socket != null;
+        socket.send(packet);
+        return socket;
+    }
+
+    /**
+     * 清除已经完成的流的记录
+     */
+    private static void cleanMap(
+            Map<Integer, TransmissionData> tDataMap,
+            Map<Integer, Integer> receiveDataMap ,
+            Map<Integer, Long> timeOutMap,
+            Map<Integer, DatagramSocket> socketMap
+    ){
+        if(timeOutMap.size() == 0)return;
+
+        // 先获取需要删除的Key
+        ArrayList<Integer> removeKey = new ArrayList<>();
+        for(int key : timeOutMap.keySet())
+            if(System.currentTimeMillis() - timeOutMap.get(key) > Constant.TIME_OUT_LIMIT)
+                removeKey.add(key);
+
+        if(removeKey.size() == 0)return; // 减少开销
+
+
+        System.out.println("Clear the record of the completed flow");
+        // 删除数据
+        for(int key : removeKey){
+            TransmissionData tData = tDataMap.remove(key);
+            receiveDataMap.remove(key);
+            timeOutMap.remove(key);
+            socketMap.remove(key).close(); // 要记得关闭socket
+            System.out.printf(
+                    "Flow(%d,%d)'record is removed\n",
+                    tData.coflow_id,tData.flow_id
+            );
         }
     }
 }
--- src/model/Constant.java
+++ src/model/Constant.java
@@ -9,12 +9,13 @@ public class Constant {
     public static final String DATA_PREFIX_SRC_PORT   = "src_port=";
     public static final String DATA_PREFIX_DST_IP     = "dst_ip=";
     public static final String DATA_PREFIX_DST_PORT   = "dst_port=";
-    public static int NUMBER_OF_DATA_PREFIX = 8;
 
+    public static int NUMBER_OF_DATA_PREFIX = 8;
     public static int BUFF_SIZE = 1024;
-    public static int CLIENT_PORT = 5002;
+//    public static int CLIENT_PORT = 5002;
     public static int SERVER_PORT = 5001;
-    public static int ACK_NUMBER = 10;
+//    public static int ACK_NUMBER = 10;
+    public static Long TIME_OUT_LIMIT = 5 * 1000L; // 单位ms
     public static int hash(int coflowID,int flowId){
         return coflowID * 60 + flowId;
     }
